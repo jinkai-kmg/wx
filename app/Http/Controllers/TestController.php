16ba49c7597a19ef358ec511d55aac36396f462c
kmg <2211071847@qq.com>
2020-11-05 20:14:58 +0800

    jk

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index(){
+        $echoStr =request()->get("echostr","");
+        if($this->checkSignature() && !empty($echoStr)){
+            //至少微信公众平台第一次接入调用走这个
+            echo $echoStr;
+
+        }
+    }
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = "shanyi";
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
