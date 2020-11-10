eae7e657d510ad157bfe2277fd91fdc616c0f3c2
kmg <2211071847@qq.com>
2020-11-10 16:05:00 +0800

    menu

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -106,8 +106,7 @@ class TestController extends Controller
         $url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$access_token;
         $client = new Client();
         $res_menu = $client->request('POST',$url,[
-            'verify'    => false,    //忽略 HTTPS证书 验证'
-
+            'verify'    => false,    //忽略 HTTPS证书 验证
             'body' => $arr
         ]);
         print_r($res_menu);
